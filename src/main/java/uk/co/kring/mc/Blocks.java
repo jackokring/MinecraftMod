59fa2caf69a1ba8b6cb7dc70a051ecf395756f69
jackokring <jackokring@gmail.com>
2020-07-18 02:10:18 +0100

    ObjectHolder

---
+++ src/main/java/uk/co/kring/mc/Blocks.java
@@ -0,0 +1,9 @@
+package uk.co.kring.mc;
+
+import net.minecraft.block.Block;
+import net.minecraftforge.registries.ObjectHolder;
+
+@ObjectHolder(value="jacko")
+public class Blocks {
+    public static Block unlock;
+}
--- src/main/java/uk/co/kring/mc/Jacko.java
+++ src/main/java/uk/co/kring/mc/Jacko.java
@@ -25,6 +25,8 @@ import org.apache.logging.log4j.Logger;
 
 import java.util.stream.Collectors;
 
+import static uk.co.kring.mc.Blocks.unlock;
+
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("jacko")
 public class Jacko
@@ -46,8 +48,7 @@ public class Jacko
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void setup(final FMLCommonSetupEvent event)
-    {
+    private void setup(final FMLCommonSetupEvent event) {
         // some preinit code
         LOGGER.info("HELLO FROM PREINIT");
         LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
@@ -56,21 +57,23 @@ public class Jacko
     private void doClientStuff(final FMLClientSetupEvent event) {
         // do something that can only be done on the client
         LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().gameSettings);
+        //IMC supplier and field
     }
 
-    private void enqueueIMC(final InterModEnqueueEvent event)
-    {
+    private void enqueueIMC(final InterModEnqueueEvent event) {
         // some example code to dispatch IMC to another mod
         InterModComms.sendTo("jacko", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
+        //binding element manufacture to supply
     }
 
-    private void processIMC(final InterModProcessEvent event)
-    {
+    private void processIMC(final InterModProcessEvent event) {
         // some example code to receive and process InterModComms from other mods
         LOGGER.info("Got IMC {}", event.getIMCStream().
-                map(m->m.getMessageSupplier().get()).
+                map(m -> m.getMessageSupplier().get()).
                 collect(Collectors.toList()));
+        //collect all supplied as a list
     }
+
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(FMLServerStartingEvent event) {
@@ -82,31 +85,26 @@ public class Jacko
     // Event bus for receiving Registry Events)
     @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
     public static class RegistryEvents {
+
         @SubscribeEvent
-        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
-            // register a new block here
-            LOGGER.info("HELLO from Register Block");//TODO??
+        public static void registerBlocks(RegistryEvent.Register<Block> event) {
+            Block unlock = new OreBlock(Block.Properties.create(Material.ROCK)
+                    .hardnessAndResistance(3f, 3f) //emerald level
+                    .sound(SoundType.STONE)
+                    .slipperiness(0.5f)
+                    .harvestTool(ToolType.PICKAXE)
+            );
+            unlock.setRegistryName("jacko", "unlock");
+            //see @ObjectHolder in uk.co.kring.mc.Blocks field import static
+            event.getRegistry().register(unlock);
         }
-    }
 
-    Block unlock = new OreBlock(Block.Properties.create(Material.ROCK)
-            .hardnessAndResistance(3f, 3f) //emerald level
-            .sound(SoundType.STONE)
-            .slipperiness(0.5f)
-            .harvestTool(ToolType.PICKAXE)
-    );
-
-    @SubscribeEvent
-    public void registerBlocks(RegistryEvent.Register<Block> event) {
-        unlock.setRegistryName("jacko", "unlock");
-        event.getRegistry().register(unlock);
-    }
-
-    @SubscribeEvent
-    public void registerBlockItems(RegistryEvent.Register<Item> event) {
-        Item.Properties itemP = new Item.Properties().group(ItemGroup.MATERIALS);
-        BlockItem unlockItem = new BlockItem(unlock, itemP);
-        unlockItem.setRegistryName(unlock.getRegistryName());
-        event.getRegistry().register(unlockItem);
+        @SubscribeEvent
+        public static void registerItems(RegistryEvent.Register<Item> event) {
+            Item.Properties itemP = new Item.Properties().group(ItemGroup.MATERIALS);
+            BlockItem unlockItem = new BlockItem(unlock, itemP);
+            unlockItem.setRegistryName(unlock.getRegistryName());
+            event.getRegistry().register(unlockItem);
+        }
     }
 }
