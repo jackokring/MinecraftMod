fbf02220d5567e24ea7df945064837eea5f273bc
jackokring <jackokring@gmail.com>
2020-07-25 22:51:31 +0100

    blocks v1 limit

--- src/main/java/uk/co/kring/mc/Blocks.java
+++ src/main/java/uk/co/kring/mc/Blocks.java
@@ -5,6 +5,11 @@ import net.minecraftforge.registries.ObjectHolder;
 
 @ObjectHolder(value="jacko")
 public class Blocks {
+    //version 1.0
     public static Block unlock;
     public static Block sigma;
+    public static Block delta;
+    public static Block upsilon;
+    public static Block pi;
+    //version 2.0
 }
